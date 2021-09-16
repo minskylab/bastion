c916033d9fb29e1e3ec5b2096780bb1554e68465
Bregy Malpartida <bregy@minsky.cc>
2021-09-16 03:52:18 -0500

    feat: fixed docker compose and adding golang to development stage

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
+FROM golang:1.17 AS golang
 FROM node:16
 
 WORKDIR /usr/src/app
+
+COPY --from=golang /usr/bin/go /usr/bin/go
 COPY . .
 
 RUN yarn
---
+++ prisma/seed.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from ".prisma/client";
+
+const client = new PrismaClient();
+
+const organizationID = "organizationID";
+
+client.project
+  .create({
+    data: {
+      name: "Project 1",
+      estimatedDuration: 24 * 30 * 3,
+      startAt: new Date(),
+      organization: {
+        connect: {
+          id: organizationID,
+        },
+      },
+    },
+  })
+  .then((project) => {
+    console.log(`project created id: ${project.id}`);
+  });
